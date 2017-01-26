511e49d82ccb56387f763c81083da8c351c26b8c
Garry T. Williams <gtwilliams@gmail.com>
2017-01-26 11:59:52 -0500

    Updated to match what's in the browser now.

--- ars.js
+++ ars.js
@@ -4,13 +4,17 @@
 // @version    0.1
 // @description  Ars Tecnica article to black
 // @match      http://arstechnica.com/*
-// @copyright  2014, Garry T. Williams
+// @match      http://arstechnica.co.uk/*
+// @match      https://arstechnica.com/*
+// @match      https://arstechnica.co.uk/*
+// @copyright  2014, 2017 Garry T. Williams
 // ==/UserScript==
 
 (function () {
     var x = document.createElement("style");
     x.appendChild(document.createTextNode(
         'article.standalone, .article-content, .comment, .quotecontent, ' +
-	'.post-content, .article-content blockquote { color: #000000 !important; }'));
+	'.comments-row, .post-content, .article-content blockquote ' +
+	'{ color: #000000 !important; }'));
     document.getElementsByTagName("head")[0].appendChild(x);
 })();
--- wsj.js
+++ wsj.js
@@ -4,12 +4,13 @@
 // @version    0.1
 // @description  WSJ article to black
 // @match      http://www.wsj.com/*
-// @copyright  2014, Garry T. Williams
+// @copyright  2014, 2017 Garry T. Williams
 // ==/UserScript==
 
 (function () {
     var x = document.createElement("style");
     x.appendChild(document.createTextNode(
-        '.article-wrap { color: #000000 !important; }'));
+        '.article-wrap, .fyre, .fyre-comment-stream ' +
+	'{ color: #000000 !important; }'));
     document.getElementsByTagName("head")[0].appendChild(x);
 })();
