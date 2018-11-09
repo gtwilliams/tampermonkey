9652b8b26f775d6a7a49f2d4ded09d4e9ae5d45f
Garry T. Williams <Garry_Williams@comcast.com>
2018-11-09 11:53:23 -0500

    Initial revision.

---
+++ bridgecheaters.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name       Bridge Cheaters to black
+// @namespace  http://cbeyond.net/
+// @version    0.1
+// @description  Bridge Cheaters to black
+// @match      http://bridgecheaters.com/*
+// @copyright  2018 Garry T. Williams
+// ==/UserScript==
+
+(function () {
+    var x = document.createElement("style");
+    x.appendChild(document.createTextNode(
+        'body { color: #000000 !important; }'));
+    document.getElementsByTagName("head")[0].appendChild(x);
+})();
