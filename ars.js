fbedb4a6fde481c2f24b74f32b4c9fdad761b4af
Garry T. Williams <gtwilliams@gmail.com>
2020-02-12 12:59:02 -0500

    Fix opacity on comment.

--- ars.js
+++ ars.js
@@ -16,7 +16,7 @@
         'article.standalone, .article-content, .comment, .quotecontent, ' +
 	'.comments-row, .post-content, .article-content blockquote, ' +
 	'.article-single blockquote ' +
-	'{ color: #000000 !important; } .comments-row #comments-area ' +
+	'{ color: #000000 !important; } .comments-area ' +
         '#comments > li.gray > .body { opacity: 1 !important; }'));
     document.getElementsByTagName("head")[0].appendChild(x);
 })();
