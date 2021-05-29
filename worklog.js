5704a55bc116784627db36e568cbe7082cc84a53
Garry T. Williams <gtwilliams@gmail.com>
2021-05-28 21:26:56 -0400

    Make black.

--- worklog.js
+++ worklog.js
@@ -1,17 +1,20 @@
 // ==UserScript==
 // @name         Format Worklog Entries Correctly
-// @namespace    https://comcast.com/
+// @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  Nice format of log entries
-// @author       Garry T. Williams
-// @match        http://sams.cable.comcast.com/*
+// @description  try to take over the world!
+// @author       You
+// @match        https://tts-ticketviewer.apps.cloud.comcast.net/*
 // @grant        none
 // ==/UserScript==
+
 (function() {
     'use strict';
     var x = document.createElement("style");
     x.appendChild(document.createTextNode(
-        '.log { white-space: pre; ' +
-        'font-family: monospace; !important; }'));
+        'div.ticketviewer[_ngcontent-c2] ' +
+        'div.worklogs-container[_ngcontent-c2] ' +
+        'span.value.details[_ngcontent-c2] ' +
+        '{ white-space: pre; font-family: monospace; !important; }'));
     document.getElementsByTagName("head")[0].appendChild(x);
 })();
