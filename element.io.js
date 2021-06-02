0dff32f64678cdb7d99df1183942fe78aa04163d
Garry T. Williams <gtwilliams@gmail.com>
2021-06-02 15:46:33 -0400

    Add more change to black.

--- element.io.js
+++ element.io.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name       Get Rid of Grey
+// @name       Get Rid of Grey In Element
 // @namespace  http://cbeyond.net/
 // @version    0.1
 // @description  Make conversations black
@@ -10,6 +10,11 @@
 (function () {
     var x = document.createElement("style");
     x.appendChild(document.createTextNode(
-        'body { color: #000000 !important; }'));
+        'body, .mx_MessageTimestamp, .hljs-meta, ' +
+        '.mx_EventTile_body { color: #000000 !important; } ' +
+        '.mx_EventTile_content .markdown-body a.mx_GroupPill, ' +
+        '.mx_EventTile_content .markdown-body a.mx_RoomPill, ' +
+        '.mx_GroupPill, .mx_RoomPill {' +
+        'color: #fff; background-color: #2e2f32;}'));
     document.getElementsByTagName("head")[0].appendChild(x);
 })();
