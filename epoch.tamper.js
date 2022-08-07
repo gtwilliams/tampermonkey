f6ca42289eef9e3c088228501dbe55da16a67f1c
Garry T. Williams <gtwilliams@gmail.com>
2022-08-07 14:58:19 -0400

    Fix Epoch Times font-family.

---
+++ epoch.tamper.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name       epoch-font-family
+// @namespace  http://cbeyond.net/
+// @version    0.1
+// @description  Fix broken font-family on epoch Web sites
+// @match      https://www.theepochtimes.com/*
+// @author     Garry T. Williams
+// ==/UserScript==
+
+(function () {
+    var x = document.createElement("style");
+    x.appendChild(document.createTextNode(
+        'div.post_content ' +
+	'{ font-family: Georgia,"Times New Roman" !important; }'));
+    document.getElementsByTagName("head")[0].appendChild(x);
+})();
