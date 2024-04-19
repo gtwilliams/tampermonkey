8f708e1ec62ef0d160b34e9eba56b1b08961f0c9
gtwilliams <gtwilliams@gmail.com>
2024-04-19 18:50:51 -0400

    Fix URL.  Add more to make black.

--- prescriptions.tamper.js
+++ prescriptions.tamper.js
@@ -4,7 +4,7 @@
 // @version      2024-04-19
 // @description  Publix Prescriptions!
 // @author       You
-// @match        https://rx.publix.com/en/rx
+// @match        https://rx.publix.com/en/rx*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=publix.com
 // @grant        none
 // ==/UserScript==
@@ -13,9 +13,10 @@
     'use strict';
     var x = document.createElement("style");
     x.appendChild(document.createTextNode(
-        'body, h3, p, h4.rx-list-medication-name, ' +
-        '#navBottom li a, .details-link a, ' +
-        '.highlight-black .rx-item-panel a ' +
+        'body, h1, h3, p, h4.rx-list-medication-name, ' +
+        'a, #navBottom li a, .details-link a, ' +
+        '.highlight-black .rx-item-panel a, ' +
+        '.rx-patient-card-header ' +
 	'{ color: #000000 !important; }'));
     document.getElementsByTagName("head")[0].appendChild(x);
 })();
